188b41f098cda2b937e28af7874f27211361262e
mokocm <mokocmmoko@gmail.com>
2023-06-17 10:14:36 +0900

    Format

--- public/dashboard/bundle.js
+++ public/dashboard/bundle.js
@@ -6,27 +6,28 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./views/dashboard.js":
-/*!****************************!*\
+/******/ (() => {
+  // webpackBootstrap
+  /******/ var __webpack_modules__ = {
+    /***/ "./views/dashboard.js":
+      /*!****************************!*\
   !*** ./views/dashboard.js ***!
   \****************************/
-/***/ (() => {
+      /***/ () => {
+        eval("\n\n//# sourceURL=webpack://aperta/./views/dashboard.js?");
 
-eval("\n\n//# sourceURL=webpack://aperta/./views/dashboard.js?");
+        /***/
+      },
 
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./views/dashboard.js"]();
-/******/ 	
-/******/ })()
-;
+    /******/
+  };
+  /************************************************************************/
+  /******/
+  /******/ // startup
+  /******/ // Load entry module and return exports
+  /******/ // This entry module can't be inlined because the eval devtool is used.
+  /******/ var __webpack_exports__ = {};
+  /******/ __webpack_modules__["./views/dashboard.js"]();
+  /******/
+  /******/
+})();
